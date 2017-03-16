df04ba430dd26ed4ece83f378cfb665bbdb85159
Stefan Tod <s.todorov@student.fontys.nl>
2017-03-16 22:13:01 +0100

    Classes

---
+++ ProxyVirtualPatternDemo/ProxyVirtualPatternDemo/Shape.cs
@@ -0,0 +1,10 @@
+﻿using System.Drawing;
+
+namespace ProxyVirtualPatternDemo
+{
+    public interface Shape
+    {
+        void move(Point p);
+
+    }
+}
---
+++ ProxyVirtualPatternDemo/ProxyVirtualPatternDemo/Square.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProxyVirtualPatternDemo
+{
+    class Square : Shape
+    {
+        private Point MoveToPoint;
+        private Circle circle;
+
+        public Square()
+        {
+            Timer timer = new Timer(new TimerCallback(TimeProc));
+            timer.Change(5000,)
+        }
+
+        public void move(Point p)
+        {
+            
+        }
+
+        public void TimeProc(object state)
+        {
+            Timer t = (Timer) state;
+            t.
+
+
+        }
+    }
+}
